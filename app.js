b7f9f42163f97e0165caf880f02c75b8f6ebcff6
Muhammad Awais <awaistopper20@gmail.com>
2023-07-05 19:03:23 +0500

    Http module

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-const logger = require("./logger");
+const http = require("http");
 
-logger.log("message");
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    res.write("Hello World");
+    res.end();
+  }
+  if (req.url == "/api/courses") {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
+});
+
+// server.on("connection", (socket) => {
+//   console.log(socket);
+// });
+
+server.listen(3000);
+
+console.log("Listening on port 3000...");
