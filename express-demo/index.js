292f5e2afdbff3eb3579731878049d06d9982411
Muhammad Awais <awaistopper20@gmail.com>
2023-07-06 12:56:22 +0500

    Hanling put requests

--- express-demo/index.js
+++ express-demo/index.js
@@ -27,15 +27,10 @@ app.get("/api/courses/:id", (req, res) => {
 });
 
 app.post("/api/courses", (req, res) => {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
-  const { error, value } = schema.validate(req.body);
+  const { error } = validateCourse(req.body);
   if (error) {
     res.status(400).send(error.details[0].message);
-    return;
   }
-  console.log(result);
   const course = {
     id: courses.length + 1,
     name: req.body.name,
@@ -43,6 +38,27 @@ app.post("/api/courses", (req, res) => {
   courses.push(course);
   res.send(course);
 });
+
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find((course) => course.id == req.params.id);
+  if (!course) {
+    res.status(404).send("The course with the given ID was not found.");
+  }
+  const { error } = validateCourse(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+  }
+  course.name = req.body.name;
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  const { error, value } = schema.validate(req.body);
+  return { error, value };
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
