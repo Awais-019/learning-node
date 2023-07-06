e73a3ffe08751f34077b048ccdca0bbebcfecdd6
Muhammad Awais <awaistopper20@gmail.com>
2023-07-06 13:00:37 +0500

    Handling delete requests

--- express-demo/index.js
+++ express-demo/index.js
@@ -52,6 +52,17 @@ app.put("/api/courses/:id", (req, res) => {
   res.send(course);
 });
 
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((course) => course.id == req.params.id);
+  if (!course) {
+    res.status(404).send("The course with the given ID was not found.");
+  }
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+  res.send(course);
+});
+
 function validateCourse(course) {
   const schema = Joi.object({
     name: Joi.string().min(3).required(),
