69c18420f3528756d5094fe9447b8451b96f741d
Muhammad Awais <awaistopper20@gmail.com>
2023-07-10 11:00:36 +0500

    Pagination

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -39,13 +39,20 @@ async function getCourses() {
   // or
   // and
 
+  const pageNumber = 2;
+  const pageSize = 10;
+
   const courses = await Course.find({
     author: "Mosh",
     isPublished: true,
   })
-    .limit(10)
+    .skip((pageNumber - 1) * pageSize)
+    .limit(pageSize)
     .sort({ name: 1 })
-    .count();
+    .select({
+      name: 1,
+      tags: 1,
+    });
   console.log(courses);
 }
 
