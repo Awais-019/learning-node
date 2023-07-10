0ca02aa804051715255fa1758dfc1da65c6623f3
Muhammad Awais <awaistopper20@gmail.com>
2023-07-10 12:07:22 +0500

    Built-in validators

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -5,20 +5,42 @@ mongoose
   .catch((err) => console.log("Could not connect to MongoDB...", err));
 
 const courseSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    // match: /pattern/,
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ["web", "mobile", "network"],
+  },
   author: String,
   tags: [String],
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
+  price: {
+    type: Number,
+    required: function () {
+      return this.isPublished;
+    },
+    min: 10,
+    max: 200,
+  },
 });
 
 const Course = mongoose.model("Course", courseSchema);
 
 async function createCourse() {
   const course = new Course({
+    name: "Angular Course",
+    category: "-",
     author: "Mosh",
     tags: ["angular", "frontend"],
     isPublished: true,
+    price: 15,
   });
 
   try {
