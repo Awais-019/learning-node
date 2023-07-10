17c366e55305c67d497fdb39008949408ef6800b
Muhammad Awais <awaistopper20@gmail.com>
2023-07-10 10:49:43 +0500

    Querying documents

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -16,9 +16,9 @@ const Course = mongoose.model("Course", courseSchema);
 
 async function createCourse() {
   const course = new Course({
-    name: "Node.js Course",
+    name: "Angular Course",
     author: "Mosh",
-    tags: ["node", "backend"],
+    tags: ["angular", "frontend"],
     isPublished: true,
   });
 
@@ -26,4 +26,18 @@ async function createCourse() {
   console.log(result);
 }
 
-createCourse();
+async function getCourses() {
+  const courses = await Course.find({
+    author: "Mosh",
+    isPublished: true,
+  })
+    .limit(10)
+    .sort({ name: 1 })
+    .select({
+      name: 1,
+      tags: 1,
+    });
+  console.log(courses);
+}
+
+getCourses();
